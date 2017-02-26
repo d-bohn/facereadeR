b0099cda8aa70879b0046a42dc93083fd594408b
Daniel Albohn <dna5021@psu.edu>
2017-02-26 17:25:01 -0500

    updated the readfacereader function to specify type of input

--- R/read.facereader.R
+++ R/read.facereader.R
@@ -10,34 +10,58 @@
 #'
 #' @param path The complete file path to location (folder) of your text files.
 #' @param skip The number of lines in the text file to skip before the headers should be read. Defaults to 7 lines.
+#' @param detailed Boolean noting whether files to be read are detailed or state files.
 #'
 #' @return
 #' A data frame that includes output provided by FaceReader 6.xx.
 #'
 #' @examples
 #'
-#' data <- read.facereader(system.file("inst/", package="facereadeR"), skip = 6)
+#' data <- read.facereader(system.file("inst/", package="facereadeR"), skip = 6, detailed = TRUE)
 #'
 #' @export
 
-read.facereader <- function(path, skip = 7) {
+read.facereader <- function(path, skip = 6, detailed = TRUE) {
   wd <- getwd()
   setwd(path)
-  get_files <- function(filename){
-    if(file.info(filename)$size > 0){
-      ret <- read.table(filename, skip = skip, header=TRUE, sep="\t")
-      ret$Source <- filename
-      ret
+
+  if(detailed==TRUE){
+    get_files <- function(filename){
+      if(file.info(filename)$size > 0){
+        ret <- read.table(filename, skip = skip, header=TRUE, sep="\t")
+        ret$Source <- filename
+        ret
+      }
     }
+
+    # Get text files and merge them into a single data frame
+    files_list <- list.files(path = path, pattern = "*.txt")
+
+    # Merge all datasets into a single file
+    data <- plyr::ldply(files_list, get_files)
+    setwd(wd)
+    return(data)
   }
+  if(detailed==FALSE){
+    get_files <- function(filename){
+      if(file.info(filename)$size > 0){
+        ret <- read.table(filename, skip = skip, header=TRUE, sep="\t")
+        ret$Source <- filename
+        ret
+      }
+    }
 
-  # Get text files and merge them into a single data frame
-  files_list <- list.files(path = path, pattern = "*.txt")
+    # Get text files and merge them into a single data frame
+    files_list <- list.files(path = path, pattern = "*.txt")
 
-  # Merge all datasets into a single file
-  data <- plyr::ldply(files_list, get_files)
-  setwd(wd)
-  return(data)
+    # Merge all datasets into a single file
+    data <- plyr::ldply(files_list, get_files)
+    setwd(wd)
+    return(data)
+  }
+  else{
+    print('Please specify what type of file you want read')
+  }
 }
 
 
