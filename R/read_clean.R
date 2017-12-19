00e9152e012046ef95ce461a021316c0a902d3e2
Daniel Albohn <dna5021@psu.edu>
2017-12-19 16:51:17 -0500

    Updated read_clean functions
    Better estimation of where the data starts

--- R/read_clean.R
+++ R/read_clean.R
@@ -5,7 +5,7 @@
 #' If \code{NULL} (default), the function will utilize the file name as the unique
 #' identifier (different from \code{source} which is derived from inside the file).
 #' @param skip Number of lines to skip before actual data is present in the file.
-#' If \code{NULL} (defalt), the function will attempt to find where the
+#' If \code{NULL} (default), the function will attempt to find where the
 #' actual data start and estimate based off of front matter how many lines to skip
 #' when reading in the file.
 #' @param ... Additional arguments to be passed to \code{\link[utils]{read.table}}
@@ -21,12 +21,27 @@
 
 read_facereader <- function(file, subject = NULL, skip = NULL, ...){
 
-  src <- read.table(file, sep="\t", as.is = TRUE, strip.white = TRUE)[1:2]
-  source <- as.character(src[src$V1=='Filename:',2])
+  src <- read.table(file, sep="\t", as.is = TRUE, strip.white = TRUE, fill=TRUE)[1:10]
+
+  position <- as.numeric(rownames(src[grep("Filename", src[,1]), ]))+1
+
+  source <- as.character(src[position,1])
 
   if(is.null(skip)){
-    start <- grep("Video Time", src$V1)
-    skip <- start - 1
+
+    start <- NULL
+    if(purrr::is_empty(start)){
+      start <- grep("Video Time", src$V1)
+      skip <- start - 1
+    }
+
+    if(purrr::is_empty(start)){
+      start <- as.numeric(rownames(src[grep("Filename", src[,1]), ]))
+      skip <- start+1
+    }
+
+    else(message('Cannot determine start point.'))
+
   }
   if (is.null(subject)){
     subject <- file
@@ -34,12 +49,14 @@ read_facereader <- function(file, subject = NULL, skip = NULL, ...){
     subject <- stringr::str_extract(source, subject)
   }
 
-  df <- read.table(file, skip = skip, header=TRUE, sep="\t", ...)
+  df <- read.table(file, sep="\t", skip=skip, as.is = TRUE,
+                   strip.white = TRUE, fill=TRUE,
+                   header=TRUE, ...)
 
   data <- df %>%
     mutate(., subject_nr = subject,
            source = source) %>%
-    select(subject_nr, source, everything())
+    select(subject_nr, source, dplyr::everything())
   return(data)
 }
 
@@ -57,57 +74,63 @@ read_facereader <- function(file, subject = NULL, skip = NULL, ...){
 
 clean_data <- function(data, include = "All"){
   data <- data
+  vars <- names(data)
+
   if(include=="Basic"){
-    data$Neutral <- as.numeric(gsub("FIT_FAILED", NA, data$Neutral))
-    data$Happy <- as.numeric(gsub("FIT_FAILED", NA, data$Happy))
-    data$Sad <- as.numeric(gsub("FIT_FAILED", NA, data$Sad))
-    data$Angry <- as.numeric(gsub("FIT_FAILED", NA, data$Angry))
-    data$Surprised <- as.numeric(gsub("FIT_FAILED", NA, data$Surprised))
-    data$Scared <- as.numeric(gsub("FIT_FAILED", NA, data$Scared))
-    data$Disgusted <- as.numeric(gsub("FIT_FAILED", NA, data$Disgusted))
-    data$Valence <- as.numeric(gsub("FIT_FAILED", NA, data$Valence))
-    data$Arousal <- as.numeric(gsub("FIT_FAILED", NA, data$Arousal))
+
+    emotions <- c('Neutral','Happy','Sad','Angry','Surprised','Scared',
+                  'Disgusted','Valence','Arousal')
+
+    for (i in seq_along(emotions)) {
+      if(emotions[[i]] %in% vars){
+        data[,emotions[i]] <- as.numeric(ifelse(data[,emotions[j]] == 'FIT_FAILED',NA,data[,emotions[i]]))
+      } else next
+    }
+
   } else if(include =='All') {
-    data$Neutral <- as.numeric(gsub("FIT_FAILED", NA, data$Neutral))
-    data$Happy <- as.numeric(gsub("FIT_FAILED", NA, data$Happy))
-    data$Sad <- as.numeric(gsub("FIT_FAILED", NA, data$Sad))
-    data$Angry <- as.numeric(gsub("FIT_FAILED", NA, data$Angry))
-    data$Surprised <- as.numeric(gsub("FIT_FAILED", NA, data$Surprised))
-    data$Scared <- as.numeric(gsub("FIT_FAILED", NA, data$Scared))
-    data$Disgusted <- as.numeric(gsub("FIT_FAILED", NA, data$Disgusted))
-    data$Valence <- as.numeric(gsub("FIT_FAILED", NA, data$Valence))
-    data$Arousal <- as.numeric(gsub("FIT_FAILED", NA, data$Arousal))
-    data$Contempt <- as.numeric(gsub("FIT_FAILED", NA, data$Contempt))
-    data$Gender <- as.factor(gsub("FIND_FAILED", NA, data$Gender))
-    data$Gender <- as.factor(gsub("FIT_FAILED", NA, data$Gender))
-    data$Age <- as.factor(gsub("FIT_FAILED", NA, data$Age))
-    data$Age <- as.factor(gsub("FIND_FAILED", NA, data$Age))
-    data$Beard <- as.factor(gsub("FIT_FAILED", NA, data$Beard))
-    data$Beard <- as.factor(gsub("FIND_FAILED", NA, data$Beard))
-    data$Moustache <- as.factor(gsub("FIT_FAILED", NA, data$Moustache))
-    data$Moustache <- as.factor(gsub("FIND_FAILED", NA, data$Moustache))
-    data$Ethnicity <- as.factor(gsub("FIT_FAILED", NA, data$Ethnicity))
-    data$Ethnicity <- as.factor(gsub("FIND_FAILED", NA, data$Ethnicity))
-    data$Y...Head.Orientation <- as.numeric(gsub("FIT_FAILED", NA,
-                                                 data$Y...Head.Orientation))
-    data$X...Head.Orientation <- as.numeric(gsub("FIT_FAILED", NA,
-                                                 data$X...Head.Orientation))
-    data$Z...Head.Orientation <- as.numeric(gsub("FIT_FAILED", NA,
-                                                 data$Z...Head.Orientation))
-    data$Mouth <- as.factor(gsub("FIT_FAILED", NA, data$Mouth))
-    data$Mouth <- as.factor(gsub("FIND_FAILED", NA, data$Mouth))
-    data$Left.Eye <- as.factor(gsub("FIT_FAILED", NA, data$Left.Eye))
-    data$Left.Eye <- as.factor(gsub("FIND_FAILED", NA, data$Left.Eye))
-    data$Right.Eye <- as.factor(gsub("FIT_FAILED", NA, data$Right.Eye))
-    data$Right.Eye <- as.factor(gsub("FIND_FAILED", NA, data$Right.Eye))
-    data$Left.Eyebrow <- as.factor(gsub("FIT_FAILED", NA, data$Left.Eyebrow))
-    data$Left.Eyebrow <- as.factor(gsub("FIND_FAILED", NA, data$Left.Eyebrow))
-    data$Right.Eyebrow <- as.factor(gsub("FIT_FAILED", NA, data$Right.Eyebrow))
-    data$Right.Eyebrow <- as.factor(gsub("FIND_FAILED", NA, data$Right.Eyebrow))
-  }else{
+
+    nums <- c('Neutral','Happy','Sad','Angry','Surprised','Scared',
+                 'Disgusted','Valence','Arousal', 'Contempt',
+                 'Y...Head.Orientation','X...Head.Orientation',
+                 'Z...Head.Orientation')
+    facts <- c('Gender','Age','Beard','Moustache','Ethnicity',
+               'Mouth','Left.Eye','Right.Eye','Left.Eyebrow',
+               'Right.Eyebrow','Glasses','Landmarks','Stimulus','Event.Marker')
+
+    for (i in seq_along(nums)) {
+      if(nums[[i]] %in% vars){
+        data[,nums[i]] <- as.numeric(ifelse(data[,nums[i]] == 'FIT_FAILED',NA,data[,nums[i]]))
+      } else next
+    }
+
+    for (j in seq_along(facts)) {
+      if(facts[[j]] %in% vars){
+
+        data[,facts[j]] <- as.character(data[,facts[j]])
+        data[,facts[j]] <- ifelse(data[,facts[j]] == 'FIT_FAILED',NA, data[,facts[j]])
+        data[,facts[j]] <- as.factor(data[,facts[j]])
+
+        } else next
+    }
+
+    for (k in seq_along(facts)) {
+      if(facts[[k]] %in% vars){
+
+        data[,facts[k]] <- as.character(data[,facts[k]])
+        data[,facts[k]] <- ifelse(data[,facts[k]] == 'FIND_FAILED',NA, data[,facts[k]])
+        data[,facts[k]] <- as.factor(data[,facts[k]])
+
+      } else {
+        next
+      }
+    }
+
+  } else{
     message('Please specify what to include in cleaning process.')
   }
+
   return(data)
+
 }
 
 #' Rename Action Units to their shortened names
@@ -118,31 +141,23 @@ clean_data <- function(data, include = "All"){
 #' @export
 #'
 #' @examples
+#' @importFrom dplyr select rename_
+#' @importFrom purrr set_names
 
 rename_aus <- function(data){
-  attach(data)
-  names(data)[names(data)=="Action.Unit.01...Inner.Brow.Raiser"] <- "AU01"
-  names(data)[names(data)=="Action.Unit.02...Outer.Brow.Raiser"] <- "AU02"
-  names(data)[names(data)=="Action.Unit.04...Brow.Lowerer"] <- "AU04"
-  names(data)[names(data)=="Action.Unit.05...Upper.Lid.Raiser"] <- "AU05"
-  names(data)[names(data)=="Action.Unit.06...Cheek.Raiser"] <- "AU06"
-  names(data)[names(data)=="Action.Unit.07...Lid.Tightener"] <- "AU07"
-  names(data)[names(data)=="Action.Unit.09...Nose.Wrinkler"] <- "AU09"
-  names(data)[names(data)=="Action.Unit.10...Upper.Lip.Raiser"] <- "AU10"
-  names(data)[names(data)=="Action.Unit.12...Lip.Corner.Puller"] <- "AU12"
-  names(data)[names(data)=="Action.Unit.14...Dimpler"] <- "AU14"
-  names(data)[names(data)=="Action.Unit.15...Lip.Corner.Depressor"] <- "AU15"
-  names(data)[names(data)=="Action.Unit.17...Chin.Raiser"] <- "AU17"
-  names(data)[names(data)=="Action.Unit.18...Lip.Puckerer"] <- "AU18"
-  names(data)[names(data)=="Action.Unit.20...Lip.Stretcher"] <- "AU20"
-  names(data)[names(data)=="Action.Unit.23...Lip.Tightener"] <- "AU23"
-  names(data)[names(data)=="Action.Unit.24...Lip.Pressor"] <- "AU24"
-  names(data)[names(data)=="Action.Unit.25...Lips.Part"] <- "AU25"
-  names(data)[names(data)=="Action.Unit.26...Jaw.Drop"] <- "AU26"
-  names(data)[names(data)=="Action.Unit.27...Mouth.Stretch"] <- "AU27"
-  names(data)[names(data)=="Action.Unit.43...Eyes.Closed"] <- "AU43"
-  detach(data)
-  return(data)
+
+  old <- data %>% dplyr::select(dplyr::starts_with('Action.')) %>%
+    names(.)
+
+  new <- c(
+    "AU01","AU02","AU04","AU05","AU06","AU07","AU09","AU10","AU12","AU14","AU15","AU17",
+    "AU18","AU20","AU23","AU24","AU25","AU26","AU27","AU43"
+  )
+
+  new_names <- purrr::set_names(old, new)
+
+  data <- data %>% dplyr::rename_(.dots=new_names)
+
 }
 
 #' Score Action Units numerically
@@ -151,7 +166,8 @@ rename_aus <- function(data){
 #'
 #' @return
 #' @export
-#' @importFrom car recode
+#' @importFrom dplyr recode
+#' @importFrom purrr map_df
 #'
 #' @examples
 
@@ -161,9 +177,9 @@ score_aus <- function(data){
     "AU18","AU20","AU23","AU24","AU25","AU26","AU27","AU43"
   )
 
-  data[AUs] <- lapply(data[AUs],
-                      function(x) car::recode(x, recodes = "'A'=1; 'B'=2; 'C'=3; 'D'=4;
-                                              'E'=5;'NotActive'=0; else=NA;", as.factor.result = FALSE))
+  data[AUs] <- purrr::map_df(data[AUs], ~dplyr::recode(.x, A=1,B=2,C=3,D=4,E=5,
+                                                       NotActive=0))
+
   return(data)
 }
 
